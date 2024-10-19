91c44cb0815689bea29dd4d84b2650088bd84983
Eswin Poroj <eporojc1@miumg.edu.gt>
2024-10-18 19:39:31 -0600

    Update: Agregue la pantalla de Mediana y Moda

--- lib/screens/mediana_screen.dart
+++ lib/screens/mediana_screen.dart
@@ -1,3 +1,4 @@
+import 'package:estadistica/widgetes/scaffold.dart';
 import 'package:flutter/material.dart';
 
 import '../widgetes/drawer.dart';
@@ -10,6 +11,176 @@ class MedianaScreen extends StatefulWidget {
 }
 
 class _MedianaScreenState extends State<MedianaScreen> {
+  List<TextEditingController> controller = [
+    TextEditingController(),
+  ];
+
+  @override
+  void dispose() {
+    for (var controller in controller) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _addTextFieldController() {
+    setState(() {
+      controller.add(TextEditingController());
+    });
+  }
+
+  void _deleteTextFieldController(int index) {
+    setState(() {
+      controller.removeAt(index);
+    });
+  }
+
+  void calcularMediana() {
+    try {
+      List<double> numeros = [];
+
+      for (var datos in controller) {
+        if (datos.text.isEmpty) {
+          mensaje('Por Favor Llene Todos Los Campos', context);
+          return;
+        }
+        numeros.add(double.parse(datos.text));
+      }
+
+      if (controller.length < 2) {
+        mensaje('Debe Ingresar Al Menos Dos Datos', context);
+        return;
+      }
+
+      /// Ordana los numeros ingresados de forma acendente
+      /// Metodo de Dart
+
+      if (numeros.length % 2 == 0) {
+        /// dividimos el tamaño de la lista entre 2
+        /// y le quitamos cualquier decimal
+        numeros.sort();
+        int mitad = numeros.length ~/ 2;
+
+        double mediana = (numeros[mitad - 1] + numeros[mitad]) / 2;
+
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Resultado'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'La Mediana de los Numeros:',
+                  ),
+                  Text(
+                    numeros.join(', '),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'La Mediana es:',
+                  ),
+                  Text(
+                    mediana.toStringAsFixed(2),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      controller.clear();
+                      controller.add(TextEditingController());
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Calcular Otros Datos'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Aceptar'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        /// dividimos el tamaño de la lista entre 2
+        /// y le quitamos cualquier decimal
+        numeros.sort();
+        int mitad = (numeros.length) ~/ 2;
+
+        print('la mitad es $mitad');
+
+        double mediana = numeros[mitad];
+
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Resultado'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'La Mediana de los Numeros:',
+                  ),
+                  Text(
+                    numeros.join(', '),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'La Mediana es:',
+                  ),
+                  Text(
+                    mediana.toStringAsFixed(2),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      controller.clear();
+                      controller.add(TextEditingController());
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Calcular Otros Datos'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Aceptar'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      print(e);
+      mensaje('Error En Calcular La Mediana', context);
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +189,45 @@ class _MedianaScreenState extends State<MedianaScreen> {
         elevation: 20,
       ),
       drawer: drawerApp(context),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              for (var i = 0; i < controller.length; i++)
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: controller[i],
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Numero ${i + 1}',
+                      border: OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          _deleteTextFieldController(i);
+                        },
+                        icon: const Icon(Icons.delete),
+                      ),
+                    ),
+                  ),
+                ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  calcularMediana();
+                },
+                child: const Text('Calcular Mediana'),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTextFieldController,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
--- lib/screens/moda_screen.dart
+++ lib/screens/moda_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgetes/drawer.dart';
+import '../widgetes/scaffold.dart';
 
 class ModaScreen extends StatefulWidget {
   const ModaScreen({super.key});
@@ -10,6 +11,131 @@ class ModaScreen extends StatefulWidget {
 }
 
 class _ModaScreenState extends State<ModaScreen> {
+  List<TextEditingController> controllers = [
+    TextEditingController(),
+  ];
+
+  @override
+  void dispose() {
+    for (var controller in controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _addTextFieldController() {
+    setState(() {
+      controllers.add(TextEditingController());
+    });
+  }
+
+  void _deleteTextFieldController(int index) {
+    setState(() {
+      controllers.removeAt(index);
+    });
+  }
+
+  void calcularModa() {
+    try {
+      List<double> numeros = [];
+
+      for (var dato in controllers) {
+        if (dato.text.isEmpty) {
+          mensaje('Todos Los Campos Deben Estar Llenos', context);
+          return;
+        }
+        numeros.add(double.parse(dato.text));
+      }
+
+      if (controllers.length < 2) {
+        mensaje('Debe Ingresar Al Menos 2 Datos', context);
+        return;
+      }
+
+      numeros.sort();
+
+      int contador = 0;
+
+      int modaMaxima = 0;
+
+      List<double> moda = [];
+
+      for (var i = 0; i < numeros.length; i++) {
+        contador = 0;
+        for (var j = 0; j < numeros.length; j++) {
+          if (numeros[i] == numeros[j]) {
+            contador++;
+          }
+        }
+
+        if (contador > modaMaxima) {
+          modaMaxima = contador;
+          moda.clear();
+          moda.add(numeros[i]);
+        } else if (contador == modaMaxima) {
+          moda.add(numeros[i]);
+        }
+      }
+
+      if (numeros.length == moda.length) {
+        mensaje(
+            'No Existe Moda o Ingreso Un Mismo Numero Varias Veces', context);
+        return;
+      }
+
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Resultado'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('La Moda de los Numeros:'),
+                Text(
+                  numeros.join(', '),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text('La Moda es:'),
+                Text(
+                  moda[0].toString(),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    controllers.clear();
+                    controllers.add(TextEditingController());
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Calcular Otros Datos'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Aceptar'),
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      print(e);
+      mensaje('Error En Calcular La Moda', context);
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +144,46 @@ class _ModaScreenState extends State<ModaScreen> {
         elevation: 20,
       ),
       drawer: drawerApp(context),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              for (var i = 0; i < controllers.length; i++)
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: controllers[i],
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Numero ${i + 1}',
+                      border: const OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          _deleteTextFieldController(i);
+                        },
+                        icon: const Icon(Icons.delete),
+                      ),
+                    ),
+                  ),
+                ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  calcularModa();
+                },
+                child: const Text('Calcular Moda'),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addTextFieldController();
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
